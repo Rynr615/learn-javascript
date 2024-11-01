a1d03b6d4b8d2aa5caae4c643592306049e80a93
Ryan Yanuar Pradana <ryanyanuarpradana@gmail.com>
2024-11-01 21:25:27 +0700

    day 1 learn jsht

---
+++ day1/angka.js
@@ -0,0 +1,12 @@
+let angkaInt = 1234567890;
+let angkaFloat = 3.14;
+let x = 8;
+
+console.log(angkaInt);
+console.log(angkaFloat);
+console.log(123e5);
+console.log(2/0);
+
+console.log(typeof(angkaFloat));
+
+console.log((x % 2 === 0) ? "genap" : "ganjil");
---
+++ day1/string.js
@@ -0,0 +1,2 @@
+let text1 = '"Gerakan Pungut Sampah" dilaksanakan pada hari jum\'at';
+console.log(text1);
