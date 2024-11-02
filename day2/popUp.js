133043889cde47e3955cc522e41387cf0d068721
Ryan Yanuar Pradana <ryanyanuarpradana@gmail.com>
2024-11-02 21:17:30 +0700

    day 2 learn jsht

---
+++ day2/popUp.js
@@ -0,0 +1,7 @@
+// alert('Hello Wangsaff'); popup box pertama
+// let x = prompt('Masukkan nama:');
+
+// console.log(x);
+
+let y = confirm('Kamu Yakin???');
+console.log(y);
---
+++ day2/variable.js
@@ -0,0 +1,12 @@
+var x; // deklarasi & inisialisasi
+
+x = 20; // assignment
+
+// var
+// let
+// const
+
+// deklarasi variable
+// var <nama variable>
+
+console.log("Isi dari variable x = " + x);
