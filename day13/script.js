c207e19ca73b2f9aa26f1b5b03cc4db626101426
Ryan Yanuar Pradana <ryanyanuarpradana@gmail.com>
2024-11-13 22:23:23 +0700

    day 13 learn jsht

---
+++ day13/script.js
@@ -0,0 +1,8 @@
+// DOM selection
+// getElementById()
+
+const judul = document.getElementById('judul');
+
+judul.style.color = 'red';
+judul.style.backgroundColor = '#ccc';
+judul.innerHTML = "Sandhika";
